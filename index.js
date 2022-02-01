bce0ddde937543d81c4f60e0b203dfee1b82e685
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2022-02-01 14:31:26 -0500

    commit tree 74b546854e9362d52e7966cbb15335a0fea57b9b

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const { reporters } = require("mocha");
+
 // Add your code here
+function submitData(uName, uEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: uName,
+            email: uEmail,
+        }),
+    })
+    .then(res => res.json())
+    .then(data => addContact(data.id))
+    .catch(() => errorMessage("Unauthorized Access"));
+}   
+
+function addContact(id){
+    document.getElementsByTagName("body")[0].innerHTML = id;
+}
+
+function errorMessage(error){
+    document.getElementsByTagName("body")[0].innerHTML = error
+
+}
